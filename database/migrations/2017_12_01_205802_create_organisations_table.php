2a2ceb1d6a3910ae0d17cb0a65c719c61a2b9525
KirstyWright <kirsty@evecmdr.com>
2017-12-01 21:08:57 +0000

    Removed account. Added organisation

--- database/migrations/2017_12_01_205802_create_organisations_table.php
+++ database/migrations/2017_12_01_205802_create_organisations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateOrganisationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('organisations', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+        });
+        Schema::create('organisations_users', function (Blueprint $table) {
+            $table->integer('organisation_id');
+            $table->integer('user_id');
+            $table->index('organisation_id');
+            $table->index('user_id');
         });
     }
 
@@ -30,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('organisations');
     }
 }
